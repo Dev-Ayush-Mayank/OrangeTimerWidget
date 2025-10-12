143c39f92abd3662022b0cf638ae451dd83f2285
Mayush03 <ayushmayank20@gmail.com>
2025-10-12 20:42:18 +0530

    removed widget 3rd party

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,7 +6,6 @@ export const Home = () => {
             <HeaderLanding showLogin={true} showRegister={true} showNavItems={true} />
             <main>
                 <h1>Welcome to the Home Page</h1>
-                <div className="elfsight-app-7ebe1ac2-10c6-4b6c-9b69-6933c2584be5" data-elfsight-app-lazy></div>
             </main>
         </div>
     )
